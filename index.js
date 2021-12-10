9d9bca55db3b22f602f79eb6a9ec2b883e54a91e
SpongeBed <56435044+SpongeBed81@users.noreply.github.com>
2021-12-11 00:52:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,73 +1,74 @@
-module.exports = {
-    //jQuery <3
-    //SpongeBed Was here...
-    KullaniciBilgi: async function(kullaniciadi) {
-        var hooman = require("hooman")
-        if(!kullaniciadi) throw new TypeError("Kullanıcı Verisinin Çekilebilmesi İçin Bir Kullanıcı Adı Girilmelidir!")
-        const response = await hooman.get(`https://www.craftrise.com.tr/oyuncu/${kullaniciadi}`)
-      const source = await response.body
-      const cheerio = require("cheerio")
-      var $ = cheerio.load(source)
-      var rankçek = $("body > div.profileTitle > div.profileTitle-generalInformation > div:nth-child(3) > p").text()
-      var yetki = $("body > div.profileTitle > div.profileTitle-generalInformation > div:nth-child(2) > p").text()
-      var aktiflik = $("body > div.profileTitle > div.buttons.align-items-parent > div > a > p").text()
-      var kayıttarih = $("#sidebar > div.rise-table.generalInformations > table > tbody > tr:nth-child(1) > td:nth-child(2)").text()
-      var songiriş = $("#sidebar > div.rise-table.generalInformations > table > tbody > tr:nth-child(2) > td:nth-child(2)").text()
-      var coinler = $("#sidebar > div.rise-table.generalInformations > table > tbody > tr:nth-child(3) > td:nth-child(2)").text()
-      var arksayısı = $("#sidebar > div.rise-table.generalInformations > table > tbody > tr:nth-child(4) > td:nth-child(2)").text()
-      var profilimg = $("body > div.profileTitle > img").attr("src")
+const hooman = require("hooman");
+const cheerio = require("cheerio");
 
- 
-      var oynadığıoyun = $("body > div.profileTitle > div.buttons.align-items-parent > div").attr("title")
-      var oyun = null
-      if(oynadığıoyun === undefined) {
-       oyun = "Oyun Oynamıyor"
-      } else {
-        oyun = oynadığıoyun.slice(10).replace("</b>", "")
-      }
-      var seviyetitle = $("body > div.profileTitle > div.profileTitle-generalInformation > div:nth-child(3)").attr("title")
-      //console.log(seviyetitle)
-      var isim = $("body > div.profileTitle > div.profileTitle-generalInformation > p").text()
-      var tp1 = seviyetitle.split("<b>").join("")
-      var tp2 = tp1.indexOf("Tecrübe puanı")
-      var tp3 = tp1.slice(tp2)
-      function reverse(s){
-        return s.split("").reverse().join("");
-    }
-    var tp4 = reverse(tp3)
-    var tp5 = tp4.indexOf(" ")
-    var tp6 = tp4.slice(tp5)
-    var tp7 = reverse(tp6)
-    var tp8 = tp7.split("Tecrübe puanı: ").join("")
-    var indexal9 = tp8.indexOf("TP")
-    var indexal10 = tp8.indexOf("Sonraki")
-    var kesme = tp8.slice(indexal10).split("Sonraki seviye: ").join("")
-    var yinesubstrbruh = kesme.substring(0, kesme.length - 1)
-    var substryapma = tp8.substring(0, indexal9-1)
-      var jsonret = {Ad: isim, ProfilResmi: profilimg, Tag: yetki ,Seviye: rankçek, TP: substryapma, SonrakiSeviyeIcinTp: yinesubstrbruh, AktifMi: aktiflik, OynadigiOyun: oyun, KayitTarihi: kayıttarih, SonGiris: songiriş, Coinler: coinler, ArkadasSayisi: arksayısı}
-      return jsonret
+module.exports = {
+    KullaniciBilgi: async function (kullaniciadi) {
+        if (!kullaniciadi)
+            throw new Error("Kullanıcı Verisinin Çekilebilmesi İçin Bir Kullanıcı Adı Girilmelidir!");
+        const response = await hooman.get(`https://www.craftrise.com.tr/oyuncu/${kullaniciadi}`);
+        const source = await response.body;
+        const $ = cheerio.load(source);
+        let status = $("body > div.profile-title > div.buttons.align-items-parent > div > a > p").text();
+        const rank = $("body > div.profile-title > div.profile-title-generalInformation > div:nth-child(3) > p").text();
+        const name = $("body > div.profile-title > div.profile-title-generalInformation > p").text();
+        const exp = $("body > div.profile-title > div.profile-title-generalInformation > div:nth-child(3)").attr("title").replaceAll("<b>", "").replaceAll("</b>", "").replaceAll("<br>", "").slice(8 + rank.length).trim()
+        const exp2 = exp.indexOf("TP")
+        const exp3 = exp.substring(0, exp2).replace("Tecrübe puanı: ", "")
+        const expfornextlevel = exp.slice(exp2 + 2).replace("Sonraki seviye: ", "").replace("TP", "")
+        const tag = $("body > div.profile-title > div.profile-title-generalInformation > div:nth-child(2) > p").text()
+        const pfp = $("body > div.profile-title > img").attr("src")
+        const gameStats = $(".gameStats").text();
+        const registered = $("#sidebar > div.rise-table.generalInformations > table > tbody > tr:nth-child(1) > td:nth-child(2)").text();
+        const last_seen = $("#sidebar > div.rise-table.generalInformations > table > tbody > tr:nth-child(2) > td:nth-child(2)").text();
+        const friends = $("#sidebar > div.rise-table.generalInformations > table > tbody > tr:nth-child(4) > td:nth-child(2)").text();
+        const coins = $("#sidebar > div.rise-table.generalInformations > table > tbody > tr:nth-child(3) > td:nth-child(2)").text().trim()
+        let gameplaying = $("body > div.profileTitle > div.buttons.align-items-parent > div").attr("title")
+        let game = null;
+        if (gameplaying === undefined) {
+            game = "Oyun Oynamıyor"
+        } else {
+            game = gameplaying.slice(10).replace("</b>", "")
+        }
+
+        if (status == "")
+            status = "Unknown";
+        else
+            status = status.trim();
+
+        const returnobject = {
+            "Ad": name,
+            "ProfilResmi": pfp,
+            "Tag": tag,
+            "Seviye": rank,
+            "TP": exp3,
+            "SonrakiSeviyeIcinTp": expfornextlevel,
+            "AktifMi": status,
+            "OynadigiOyun": game,
+            "KayitTarihi": registered,
+            "SonGiris": last_seen,
+            "Coinler": coins,
+            "ArkadasSayisi": friends
+        };
+        return returnobject;
     },
     OyunlarBilgisiniAl: async function(kullaniciadi, oyunadi) {
-      var hooman = require("hooman")
-      if(!kullaniciadi) throw new TypeError("Kullanıcı Verisinin Çekilebilmesi İçin Bir Kullanıcı Adı Girilmelidir!")
-      if(!oyunadi) throw new TypeError("Kişinin Oyun Hakkındaki İstatistiklerini Öğrenmek İçin Geçerli Bir Oyun Adı Girmelisin!")
-      const response = await hooman.get(`https://www.craftrise.com.tr/oyuncu/${kullaniciadi}`)
-      const source = await response.body
-      const cheerio = require("cheerio")
-      var $ = cheerio.load(source)
-      var games = $(".gameStats").text()
-      var games2 = games.replace(/\r\n/g,'\n').split('\n');
-      games2 = games2.filter(item => item);
-      var count = 0
-      for(element of games2) {
-        var elemental = element.toLowerCase()
-        if(elemental === oyunadi.toLowerCase()) {
-          var kazanma= games2[count+2]
-          var puan= games2[count+4]
-          var jsonret = {Kazanma: kazanma, Puan: puan}
-          return jsonret
-        }
-        count++
+        if(!kullaniciadi) throw new TypeError("Kullanıcı Verisinin Çekilebilmesi İçin Bir Kullanıcı Adı Girilmelidir!")
+        if(!oyunadi) throw new TypeError("Kişinin Oyun Hakkındaki İstatistiklerini Öğrenmek İçin Geçerli Bir Oyun Adı Girmelisin!")
+        const response = await hooman.get(`https://www.craftrise.com.tr/oyuncu/${kullaniciadi}`)
+        const source = await response.body
+        let $ = cheerio.load(source)
+        const games = $(".gameStats").text()
+        let games2 = games.split('\n');
+       let res = []
+        games2.forEach(el => {
+            if(el !== " ") {
+                res.push(el)
+            }
+        })
+        res = res.filter(item => item);
+       const getindex = res.indexOf(oyunadi.toUpperCase())
+       const getpoints = res[getindex+4]
+       const getwins = res[getindex+2]
+       return {"Kazanma": getwins, "Puan": getpoints}
       }
-    }
 }
